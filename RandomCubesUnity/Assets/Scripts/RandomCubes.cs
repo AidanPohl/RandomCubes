ac0baf4a960e24396297bec67edfae3fe5641393
AidanPohl <98359096+AidanPohl@users.noreply.github.com>
2022-01-24 16:35:26 -0600

    RandomCubes Script
    - Created RandomCubes Prefab
    - Initialized RandomCubes Script (WIP)

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,29 @@
+/***
+ * Created by: Aidan Pohl
+ * Date Created: Jan 24, 2022
+ * 
+ * Last Edited by: N/A
+ * Last Edited: Jan 24, 2022
+ * 
+ * Description: Spawns multiple cube prefabs into scene
+ ****/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
